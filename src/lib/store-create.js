8a90e94bdce1fb72b960efdeed0d59a75ac3b5ae
mikeporter <porter.michael.james@gmail.com>
2017-08-15 10:33:46 -0700

    added redux-reporter, redux-thunk, util.js and updated store-create

--- src/lib/store-create.js
+++ src/lib/store-create.js
@@ -1,5 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
+import thunk from './redux-thunk.js';
+import reporter from './redux-reporter.js';
 
 import reducer from '../reducer';
 
-export default () => createStore(reducer);
+export default () => createStore(reducer, applyMiddleware(thunk, reporter));
