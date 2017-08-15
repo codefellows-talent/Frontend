23a662ad5d9c4c29a59bee376072b445fa6b750a
Saul Greene <spdysg@gmail.com>
2017-08-15 09:50:56 -0700

    sepersated scss files

--- src/component/landing-container/index.js
+++ src/component/landing-container/index.js
@@ -1,5 +1,7 @@
+import '../../style/_landing-container.scss'
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 
 class LandingContainer extends React.Component{
 
@@ -28,9 +30,9 @@ class LandingContainer extends React.Component{
           <img className='grad-pics' src='https://s3.amazonaws.com/codefellows-hiring-partners/michael.jpeg'/>
         </div>
         <div>
-        <img className='landing-hero' src='https://s3.amazonaws.com/codefellows-hiring-partners/Screen+Shot+2017-08-14+at+11.27.09+PM.png'/>
-        <h1 id="grad-explination">Take a Look</h1>
-        <h6 id="grad-explination-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
+          <img className='landing-hero' src='https://s3.amazonaws.com/codefellows-hiring-partners/Screen+Shot+2017-08-14+at+11.27.09+PM.png'/>
+          <h1 id="grad-explination">Take a Look</h1>
+          <h6 id="grad-explination-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
           <Link to='/talents' className='landing-talent-button'>Talent</Link>
         </div>
       </div>
