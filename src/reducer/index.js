87b88ab6b3ae222c2e64d3c5ffd7ee347739791d
Nicu Parente <nicuparente@gmail.com>
2017-08-17 16:12:09 -0700

    deleted unused files

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,5 @@
 import {combineReducers} from 'redux';
 // we'll drop this /route.js. It's just dummy data to get everything to laod
-import connectReducer from './connect.js';
 import profileReducer from './profile.js';
 
 export default combineReducers({
