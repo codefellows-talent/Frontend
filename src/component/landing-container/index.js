cf770fe5719c02c2871b1bc16f9eeebe9a73f84f
mikeporter <porter.michael.james@gmail.com>
2017-08-30 18:37:18 -0700

    updated css on page

--- src/component/landing-container/index.js
+++ src/component/landing-container/index.js
@@ -2,58 +2,60 @@ import '../../style/_landing-container.scss';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+class LandingContainer extends React.Component {
 
-class LandingContainer extends React.Component{
-
-  render(){
-    return(
+  render () {
+    return (
       <div>
         <div className='hero-container'>
-          <img className='hero-image' src='https://s3.amazonaws.com/codefellows-hiring-partners/helloquence-61189.jpg'/>
+          <img className='hero-image' src='https://s3.amazonaws.com/codefellows-hiring-partners/helloquence-61189.jpg' />
           <div className='hero-text'>
             <h1>Graduate Hiring</h1>
             <h6>Unbiased Resumes to Identify Qualified Candidates</h6>
           </div>
         </div>
 
-        <div className='student-container'>
-          <div className="item">
-            <img className='grad-pics' src='https://s3.amazonaws.com/codefellows-hiring-partners/saul.jpg'/>
-            <p>Full Stack Javascript Developer, with a focus in UI/UX design</p>
+        <div className='students'>
+          <div className='student-container'>
+            <img className='grad-pics' src='https://s3.amazonaws.com/codefellows-hiring-partners/saul.jpg' />
+            <p>Full Stack <span>Javascript</span> Developer<span>, with a focus in UI/UX design</span></p>
             <h5>Saul Greene</h5>
             <h6>Current Code Fellows Student</h6>
           </div>
-          <div className="item">
-            <img className='grad-pics' src='https://s3.amazonaws.com/codefellows-hiring-partners/felix.png'/>
-            <p>Full Stack Javascript Developer, with a focus in backend development</p>
+          <div className='student-container'>
+            <img className='grad-pics' src='https://s3.amazonaws.com/codefellows-hiring-partners/felix.png' />
+            <p>Full Stack <span>Javascript</span> Developer<span>, with a focus in backend development</span></p>
             <h5>Ryan Felix</h5>
             <h6>Current Code Fellows Student</h6>
           </div>
-          <div className="item">
-            <img className='grad-pics' src='https://s3.amazonaws.com/codefellows-hiring-partners/nicu.png'/>
-            <p>FullStack Developer that loves DevOps</p>
+          <div className='student-container'>
+            <img className='grad-pics' src='https://s3.amazonaws.com/codefellows-hiring-partners/nicu.png' />
+            <p>FullStack Developer<span> that loves DevOps</span></p>
             <h5>Nicu Parente</h5>
             <h6>Current Code Fellows Student</h6>
           </div>
-          <div className="item">
-            <img className='grad-pics' src='https://s3.amazonaws.com/codefellows-hiring-partners/michael.jpeg'/>
-            <p>Full Stack Javascript developer, with experience in Ruby on Rails.</p>
+          <div className='student-container'>
+            <img className='grad-pics' src='https://s3.amazonaws.com/codefellows-hiring-partners/michael.jpeg' />
+            <p>Full Stack <span>Javascript </span>developer<span>, with experience in Ruby on Rails.</span></p>
             <h5>Michael Porter</h5>
             <h6>Current Code Fellows Student</h6>
           </div>
         </div>
 
         <div className='hero-container'>
-          <img className='hero-image' src='https://s3.amazonaws.com/codefellows-hiring-partners/Screen+Shot+2017-08-14+at+11.27.09+PM.png'/>
-          <div>
-            <h1>Take a Look</h1>
-            <h6>
-              Resume bias is a well documented occurrence. It refers to the prejudice for or against a candidates resume based on characteristics inferred from the document. These characteristics might include the candidates name, age, gender, interests, ethnicity, social status, appearance, or any other characteristic a hiring professional might derive an inference from unintentionally.
-            </h6>
-            <h6>
-              In an effort to meet the needs of a growing tech marketplace requiring new talent, perspective, ideas and diversity we have created a 'blind' unbiased resume source. Here randomized candidates can provide raw data to be evaluated upon without many of the naturally occurring biases present in a traditional resume.
-            </h6>
-            <Link to='/talents' className='landing-talent-button'>Talent</Link>
+          <img className='hero-bottom-image' src='https://s3.amazonaws.com/codefellows-hiring-partners/Screen+Shot+2017-08-14+at+11.27.09+PM.png' />
+          <div className='hero-bottom-background'>
+            <div className='hero-bottom-text'>
+              <h1>Take a Look</h1>
+              <h6>
+                Resume bias is a well documented occurrence. It refers to the prejudice for or against a candidates resume based on characteristics inferred from the document. These characteristics might include the candidates name, age, gender, interests, ethnicity, social status, appearance, or any other characteristic a hiring professional might derive from unintentionally from a resume.
+              </h6>
+              <h6>
+                In an effort to meet the needs of a growing tech marketplace requiring new talent, perspective, ideas and diversity we have created a 'blind' unbiased resume source. Here randomized candidates can provide raw data to be evaluated upon without many of the biases present in a traditional resume.
+              </h6>
+              <br />
+              <Link to='/talents' className='landing-talent-button'>Talent</Link>
+            </div>
           </div>
         </div>
       </div>
